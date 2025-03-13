3f0e7e00832c528655ba68e7b7fc4fc9341a532f
dimichiko <ianvamvoukas1230@gmail.com>
2025-03-13 12:32:47 -0300

    Collect a bet amount

--- project.js
+++ project.js
@@ -1,4 +1,4 @@
-// 1. Deposite some money
+// 1. Deposite some money 
 // 2. Determine number of lines to bet
 // 3. Collect a bet amount
 // 4. Spin the slot machine
@@ -24,5 +24,35 @@ const deposit = () => {
      }
     }
 };
-const depositAmount = deposit();
-console.log(depositAmount);
+
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+   
+        if (isNaN(numberOfLines) ||numberOfLines <= 0 || numberOfLines > 3) {
+           console.log("Invalid number of lines, try again.");
+        } else {
+           return numberOfLines;
+        }
+    }
+};
+
+const getBet = (balance, line) => {
+    while (true) {
+        const bet = prompt("Enter the bet per line: ");
+        const numberBet = parseFloat(bet);
+   
+        if (isNaN(numberBet) ||numberBet <= 0 || numberBet > balance / line) {
+           console.log("Invalid bet amount, try again.");
+        } else {
+           return numberBet;
+        }
+    }
+};
+
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
+
