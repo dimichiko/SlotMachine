6ce44adccda7225f68f33dfc649c5a21cc7ebc77
dimichiko <ianvamvoukas1230@gmail.com>
2025-03-13 12:16:05 -0300

    Deposite the money

---
+++ project.js
@@ -0,0 +1,28 @@
+// 1. Deposite some money
+// 2. Determine number of lines to bet
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. check if the user won
+// 6. give the user the winnings
+// 7. play again
+
+// function deposit() {
+//     return
+// }
+
+const prompt = require('prompt-sync')();
+
+const deposit = () => {
+    while (true) {
+     const depositAmount = prompt("Enter a deposit amount: ");
+     const numberDepositAmount = parseFloat(depositAmount);
+
+     if (isNaN(numberDepositAmount) ||numberDepositAmount <= 0) {
+        console.log("Invalid deposit amount, try again.");
+     } else {
+        return numberDepositAmount;
+     }
+    }
+};
+const depositAmount = deposit();
+console.log(depositAmount);
